74f2f536f2157d6c0fd97e5a5025d0ecbea33fab
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-14 16:16:04 +0200

    CRUD2 - Refactored App.js to have routes, separate Home page component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+
 import './App.css';
-
-import Post from './components/Post'
-
-const mockPosts = [
-  {
-    likes: 20,
-    description: "The changed description",
-    image: {
-      url: "/uploads/Squash_750bdf0469.jpeg"
-    }
-  },
-  {
-    likes: 33,
-    description: "The second post",
-    image: {
-      url: "/uploads/Squash_750bdf0469.jpeg"
-    }
-  }
-]
 
 function App() {
 
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const response = await fetch('http://localhost:1337/posts')
-      const data = await response.json()
-      setPosts(data)
-    }
-
-    getPosts()
-  }, [])
-  
   return (
     <div className="App">
-      {posts.map(post => (
-        <Post 
-          likes={post.likes}
-          description={post.description}
-          url={post.image && post.image.url}
-        />
-      ))}
+      <h2>App</h2>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
