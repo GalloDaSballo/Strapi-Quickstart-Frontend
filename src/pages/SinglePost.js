a087d164855dae8bdf877180c180307b3141cc4f
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-16 20:03:36 +0200

    21 - VIEW ONE 3 - Delete Request

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import Post from '../components/Post'
 
-export default ({match}) => {
+export default ({match, history}) => {
 
     const {id} = match.params
     console.log("id", id)
 
     const [post, setPost] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const handleDelete = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`, {
+            method: 'DELETE'
+        })
+        const data = await response.json()
+        history.push('/')
+    }
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -15,17 +24,33 @@ export default ({match}) => {
 
             console.log("data", data)
             setPost(data)
+            setLoading(false)
         }
         fetchPost()
-    })
+    }, [])
 
     return (
         <div>
-            <Post 
-                description={post.description}
-                url={post.image && post.image.url}
-                likes={post.likes}
-            />
+            {loading &&
+                <p>Loading...</p>
+            }
+            {!loading &&
+                <>
+                    {post.id &&
+                        <>
+                            <Post 
+                                description={post.description}
+                                url={post.image && post.image.url}
+                                likes={post.likes}
+                            />
+                            <button onClick={handleDelete}>Delete this Post</button>
+                        </>
+                    }
+                    {!post.id &&
+                        <p>404 - not found</p>
+                    }
+                </>
+            }
         </div>
     )
 
