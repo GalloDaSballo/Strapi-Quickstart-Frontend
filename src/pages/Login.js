0e488256d834f8453440f14604d78f3843133e73
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-17 16:02:14 +0200

    25 - USER 3 - Login Page Form and Request + teaser for validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default () => (
-    <div>
-        Login
-    </div>
-)
+export default () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        const response = await fetch('http://localhost:1337/auth/local/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                identifier: email,
+                password
+            })
+        })
+
+        const data = await response.json()
+
+        console.log("data", data)
+        
+    } 
+
+    return (
+        <div>
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <button>Login</button>
+            </form>
+
+        </div>
+    )
+
+}
